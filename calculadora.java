187de41428b4b6b1e53389237c2e15a9db2b5bc2
Alejandra <Maria Alejandra>
2021-04-22 20:46:34 -0500

    Funciones de dividir y modulo agregadas

--- calculadora.java
+++ calculadora.java
@@ -14,6 +14,23 @@ public class calculadora{
         return a * b;
         }
 
+	public static int dividir( int a, int b){
+	if(b!=0){
+	return a/b;
+	}else{
+	return 0;}
+	}
+
+	public static int modulo(int a, int b){
+	return a%b;
+	}
+
+
+
+
+
+
+
 
 }
 
