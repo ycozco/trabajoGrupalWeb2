deb04d3439626fc73e0c8a8c7e8c761e8c84543b
unknown <eduranm@unsa.edu.pe>
2021-04-22 20:30:42 -0500

    funciones de restar y multiplicar agregadas

--- calculadora.java
+++ calculadora.java
@@ -2,9 +2,18 @@ public class calculadora{
 	public static void main(String args[]){
 	
 	}
-	public static int suma( int a, int b){
+	public static int sumar( int a, int b){
 	return a + b;
 	}
+
+	public static int restar( int a, int b){
+        return a - b;
+        }
+
+	public static int multiplicacion( int a, int b){
+        return a * b;
+        }
+
 
 }
 
